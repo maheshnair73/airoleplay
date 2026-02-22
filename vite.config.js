827da9ce33100f6086a6f09ce7b7fc3cde7f24d2
Mahesh Nair - EffyBiz <mahesh@effybiz.com>
2026-02-22 11:30:49 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
     extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
   optimizeDeps: {
+    include: ['@supabase/supabase-js'],
     esbuildOptions: {
       loader: {
         '.js': 'jsx',
