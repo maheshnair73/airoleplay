c1b47a0ce2bacfa97765b0d0dab9ff6c5bcda522
Mahesh Nair - EffyBiz <mahesh@effybiz.com>
2026-02-27 11:33:47 +0530

    Updated index.html

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true
+    allowedHosts: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Client-Info, apikey'
+    }
   },
   resolve: {
     alias: {
